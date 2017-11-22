57059443d458550c4994276f72026746783cf0db
Sebastian <anse23@hotmail.com>
2017-11-22 23:56:31 +0000

    feat: Modified script for face recognition
    The script allows training and validation

--- face_recognition.py
+++ face_recognition.py
@@ -1,8 +1,10 @@
+import argparse
 import scipy.io as sio
 import numpy as np
 from common import extract_features, extract_features_lbp
 from sklearn.svm import SVC
 from sklearn.preprocessing import Normalizer
+from sklearn.externals import joblib
 
 def load_data(pref, path):
     
@@ -39,15 +41,42 @@ def extract_features_all(X):
     return np.hstack((X_hog, X_lbp))
 
 def train_model():
-	X_train, Y_train = load_data('tr', './data/face_recognition/face_recognition_data_tr.mat')
-	X_val, Y_val = load_data('va', './data/face_recognition/face_recognition_data_va.mat')
+    X_train, Y_train = load_data('tr', './data/face_recognition/face_recognition_data_tr.mat')
+    X_val, Y_val = load_data('va', './data/face_recognition/face_recognition_data_va.mat')
 
-	Xfeat_train = extract_features_all(X_train)
-	Xfeat_val = extract_features_all(X_val)
+    Xfeat_train = extract_features_all(X_train)
+    Xfeat_val = extract_features_all(X_val)
 
-	model = SVC(kernel="linear", C=100.0)
-	model.fit(Xfeat_train, Y_train)
-	return model.score(Xfeat_val, Y_val)
+    model = SVC(kernel="linear", C=100.0)
+    model.fit(Xfeat_train, Y_train)
+
+    joblib.dump(model, 'recognition.pkl')
+
+    return model.score(Xfeat_val, Y_val)
+
+def val_model():
+    clf = joblib.load("recognition.pkl")
+    X_test, Y_test = load_data('va', './data/face_recognition/face_recognition_data_te.mat')
+
+    Xfeat_test = extract_features_all(X_test)
+
+    return clf.score(Xfeat_test, Y_test)
+
+def perform(stage):
+    if stage == 0:
+        score = train_model()
+        print("Score during training: {:.2f}".format(score))
+    else:
+        score = val_model()
+        print("Score during testing: {:.2f}".format(score))
 
 
-print(train_model())
+desc = """ SVM classifier for face recognition
+           """
+parser = argparse.ArgumentParser(description=desc)
+parser.add_argument('stage', type=int, help="Train or testing")
+
+args = parser.parse_args()
+stage = args.stage
+
+perform(stage)
