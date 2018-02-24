4b74b7a35ed5dde02136a08daff033d9a9895f40
Sebastian <anse23@hotmail.com>
2018-02-24 12:33:40 +0000

    feat: Added other files missed in prev coommit

---
+++ cav_cw1/CAV-cw1/Assets/Robot Kyle/scripts/Right_Wrist_Joint_01.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Right_Wrist_Joint_01 : MonoBehaviour {
+
+	GameObject sphere;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 target_pos = GameObject.Find("Sphere").GetComponent<Sphere>().position;
+		float alpha = GameObject.Find("Sphere").GetComponent<Sphere>().alpha;
+		Vector3 end_pos = transform.position;
+		Transform[] chain = Serializer.KinematicChainTransforms(transform,3);
+		Matrix angles = Serializer.IKJacobianTranspose(target_pos, chain, alpha);
+		// Debug.Log(""+MatrixUtility.MatrixAsString(angles.Values));
+		Serializer.UpdateJoints(angles, chain);
+	}
+
+}
---
+++ cav_cw1/CAV-cw1/Assets/Robot Kyle/scripts/Sphere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Sphere : MonoBehaviour {
+
+	public Vector3 position;
+	public float alpha = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		position = transform.position;
+	}
+}
