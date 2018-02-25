aca918f6aeb2732f801f4f47d9aa8392481df22e
Sebastian <anse23@hotmail.com>
2018-02-25 01:17:22 -0500

    feat: Implemented FK in custom GUI

--- cav_cw1/CAV-cw1/Assets/Editor/KinematicsWindow.cs
+++ cav_cw1/CAV-cw1/Assets/Editor/KinematicsWindow.cs
@@ -5,59 +5,41 @@ using System.Text.RegularExpressions;
 
 public class KinematicsWindow : EditorWindow
 {
-    bool groupEnabled;
-
-    // Shoulder variables
-    float shoulder_x = 0;
-    float shoulder_y = 0;
-    float shoulder_z = 0;
-
-    // Elbow variables
-    float elbow_x = 0;
-    float elbow_y = 0;
-    float elbow_z = 0;
-
-    // Wrist variables
-    float wrist_x = 0;
-    float wrist_y = 0;
-    float wrist_z = 0;
-    GameObject left_wrist;
-
     // Add menu item named "My Window" to the Window menu
     [MenuItem("CAV/Kinematics")]
     public static void ShowWindow()
     {
         //Show existing window instance. If one doesn't exist, make one.
 		EditorWindow.GetWindow(typeof(KinematicsWindow));
-        Debug.Log("Show");
     }
     
+	void FK(GameObject go, string label) {
+		Vector3 rotation = go.transform.localEulerAngles;
+
+		GUILayout.Label (label, EditorStyles.boldLabel);
+		// Due to the how Unity handle the rotations internally
+		// the range for X is 0-90, otherwise, unexpected behaviour
+		// occurs
+		float x = EditorGUILayout.Slider ("X", rotation.x, 0, 90);
+		float y = EditorGUILayout.Slider ("Y", rotation.y, 0, 360);
+		float z = EditorGUILayout.Slider ("Z", rotation.z, 0, 360);
+
+		go.transform.localRotation = Quaternion.identity;
+		go.transform.Rotate (new Vector3 (x, y, z));
+	}
+
     void OnGUI()
     {
         GUILayout.Label ("Options FK", EditorStyles.boldLabel);
         
-        // Sliders for shoulder
-        GUILayout.Label ("Shoulder", EditorStyles.boldLabel);
-        shoulder_x = EditorGUILayout.Slider ("X", shoulder_x, 0, 90);
-        shoulder_y = EditorGUILayout.Slider ("Y", shoulder_y, 0, 90);
-        shoulder_z = EditorGUILayout.Slider ("Z", shoulder_z, 0, 90);
-        
-        // Sliders for elbow
-        GUILayout.Label ("Elbow", EditorStyles.boldLabel);
-        elbow_x = EditorGUILayout.Slider ("X", elbow_x, 0, 90);
-        elbow_y = EditorGUILayout.Slider ("Y", elbow_y, 0, 90);
-        elbow_z = EditorGUILayout.Slider ("Z", elbow_z, 0, 90);
+        // FK for shoulder
+		FK (GameObject.Find("Left_Upper_Arm_Joint_01"), "Shoulder");
 
-        
-        left_wrist = GameObject.Find("Left_Wrist_Joint_01");
-        Quaternion rotation = left_wrist.transform.localRotation; 
-        Vector3 lw_rotation = left_wrist.transform.localEulerAngles;
-        Debug.Log(rotation.ToString());
+        // FK for elbow
+		FK (GameObject.Find("Left_Forearm_Joint_01"), "Elbow");
 
-        GUILayout.Label ("Wrist", EditorStyles.boldLabel);
-        wrist_x = EditorGUILayout.Slider ("X", lw_rotation.x, 0, 360);
-        wrist_y = EditorGUILayout.Slider ("Y", lw_rotation.y, 0, 360);
-        wrist_z = EditorGUILayout.Slider ("Z", lw_rotation.z, 0, 360);
+		// FK for wrist        
+		FK (GameObject.Find("Left_Wrist_Joint_01"), "Wrist");
 
 		// IK
 		string[] labels = new string[3];
