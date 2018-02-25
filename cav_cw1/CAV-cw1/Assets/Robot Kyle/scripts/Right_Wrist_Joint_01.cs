49c7e6e71d8909740366fa5b3fc91c8c8f532cc0
Sebastian <anse23@hotmail.com>
2018-02-25 00:30:31 -0500

    feat: Moved IK logic from wrist to sphere

--- cav_cw1/CAV-cw1/Assets/Robot Kyle/scripts/Right_Wrist_Joint_01.cs
+++ cav_cw1/CAV-cw1/Assets/Robot Kyle/scripts/Right_Wrist_Joint_01.cs
@@ -14,19 +14,6 @@ public class Right_Wrist_Joint_01 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 target_pos = GameObject.Find("Sphere").GetComponent<Sphere>().position;
-		float alpha = GameObject.Find("Sphere").GetComponent<Sphere>().alpha;
-		float lambda = GameObject.Find("Sphere").GetComponent<Sphere>().lambda;
-		int option = GameObject.Find("Sphere").GetComponent<Sphere>().option;
-		Vector3 end_effector_pos = GameObject.Find ("Right_Middle_Finger_Joint_01c").transform.position;
-		Transform[] chain = Serializer.KinematicChainTransforms(transform,3);
-
-		if (option == 0) {
-			Serializer.IKJacobianTranspose (target_pos, end_effector_pos, chain, alpha);
-		} else if (option == 1) {
-			Serializer.IKJacobianPseudoInverse (target_pos, end_effector_pos, chain);
-		} else if (option == 2) {
-			Serializer.IKJacobianDampedLeastSquares (target_pos, end_effector_pos, chain, lambda);
-		}
+		
 	}
 }
--- cav_cw1/CAV-cw1/Assets/Robot Kyle/scripts/Sphere.cs
+++ cav_cw1/CAV-cw1/Assets/Robot Kyle/scripts/Sphere.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class Sphere : MonoBehaviour {
 
-	public Vector3 position;
 	public float alpha = 16.0f;
 	public float lambda = 0.1f;
 	public int option = 0;
 
 	// Update is called once per frame
 	void Update () {
-		position = transform.position;
+		Vector3 target_pos = transform.position;
+		Vector3 end_effector_pos = GameObject.Find ("Right_Middle_Finger_Joint_01c").transform.position;
+		Transform end_effector = GameObject.Find ("Right_Wrist_Joint_01").transform;
+		Transform[] chain = Serializer.KinematicChainTransforms(end_effector,3);
+
+		if (option == 0) {
+			Serializer.IKJacobianTranspose (target_pos, end_effector_pos, chain, alpha);
+		} else if (option == 1) {
+			Serializer.IKJacobianPseudoInverse (target_pos, end_effector_pos, chain);
+		} else if (option == 2) {
+			Serializer.IKJacobianDampedLeastSquares (target_pos, end_effector_pos, chain, lambda);
+		}
 	}
 }
