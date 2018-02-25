5de33b2f0f9374a8df0352ab25106b3e5796a97b
Sebastian <anse23@hotmail.com>
2018-02-25 04:23:21 -0500

    feat: Updated FK options with text boxes and button

--- cav_cw1/CAV-cw1/Assets/Editor/KinematicsWindow.cs
+++ cav_cw1/CAV-cw1/Assets/Editor/KinematicsWindow.cs
@@ -12,42 +12,65 @@ public class KinematicsWindow : EditorWindow
         //Show existing window instance. If one doesn't exist, make one.
 		EditorWindow.GetWindow(typeof(KinematicsWindow));
     }
-    
-	void FK(GameObject go, string label) {
+
+	Vector3 FK(GameObject go, string label, Vector3 v) {
 		Vector3 rotation = go.transform.localEulerAngles;
 
 		GUILayout.Label (label, EditorStyles.boldLabel);
-		// Unity presents the X euler angle between 0 and 90
-		// and 270 and 360
-		/*float x = EditorGUILayout.Slider ("X", rotation.x, 0, 90);
-		float y = EditorGUILayout.Slider ("Y", rotation.y, 0, 360);
-		float z = EditorGUILayout.Slider ("Z", rotation.z, 0, 360);
-	*/
-		float x = EditorGUILayout.FloatField ("X: ", rotation.x);
-		float y = EditorGUILayout.FloatField ("Y: ", rotation.y);
-		float z = EditorGUILayout.FloatField ("Z: ", rotation.z);
 
+		float x = EditorGUILayout.FloatField ("X: ", v.x);
+		float y = EditorGUILayout.FloatField ("Y: ", v.y);
+		float z = EditorGUILayout.FloatField ("Z: ", v.z);
+
+		return new Vector3 (x, y, z);
+	}
+
+	void ApplyRotation(GameObject[] go, Vector3[] rot) {
+		if (GUI.Button(new Rect (160, 9, 65, 20), "Rotate") == true) {
+			for (int i = 0; i < go.Length; i++) {
+				RotateGameObject (go [i], rot [i]);
+			}
+		}
+	}
+
+	void RotateGameObject(GameObject go, Vector3 rot) {
 		go.transform.localRotation = Quaternion.identity;
-		Quaternion qx = Quaternion.AngleAxis (x, new Vector3 (1, 0, 0));
-		Quaternion qy = Quaternion.AngleAxis (y, new Vector3 (0, 1, 0));
-		Quaternion qz = Quaternion.AngleAxis (z, new Vector3 (0, 0, 1));
+		Quaternion qx = Quaternion.AngleAxis (rot.x, new Vector3 (1, 0, 0));
+		Quaternion qy = Quaternion.AngleAxis (rot.y, new Vector3 (0, 1, 0));
+		Quaternion qz = Quaternion.AngleAxis (rot.z, new Vector3 (0, 0, 1));
 		Quaternion q = qz * qy * qx;
 		go.transform.Rotate (q.eulerAngles);
 	}
 
+	Vector3 r_shoulder = Vector3.zero;
+	Vector3 r_elbow = Vector3.zero;
+	Vector3 r_wrist = Vector3.zero;
     void OnGUI()
-    {
-        GUILayout.Label ("Options FK", EditorStyles.boldLabel);
+	{
+		GUILayout.Label ("Options FK", EditorStyles.boldLabel);
         
-        // FK for shoulder
-		FK (GameObject.Find("Left_Upper_Arm_Joint_01"), "Shoulder");
+		// FK for shoulder
+		GameObject shoulder = GameObject.Find ("Left_Upper_Arm_Joint_01");
+		r_shoulder = FK (shoulder, "Shoulder", r_shoulder);
 
-        // FK for elbow
-		FK (GameObject.Find("Left_Forearm_Joint_01"), "Elbow");
+		// FK for elbow
+		GameObject elbow = GameObject.Find("Left_Forearm_Joint_01");
+		r_elbow = FK (elbow, "Elbow", r_elbow);
+	
+		// FK for wrist
+		GameObject wrist = GameObject.Find("Left_Wrist_Joint_01");
+		r_wrist = FK (wrist, "Wrist",  r_wrist);
 
-		// FK for wrist        
-		FK (GameObject.Find("Left_Wrist_Joint_01"), "Wrist");
-
+		GameObject[] gos = new GameObject[3];
+		Vector3[] rots = new Vector3[3];
+		gos [0] = shoulder;
+		rots [0] = r_shoulder;
+		gos [1] = elbow;
+		rots [1] = r_elbow;
+		gos [2] = wrist;
+		rots [2] = r_wrist;
+	
+		ApplyRotation (gos, rots);
 		// IK
 		string[] labels = new string[3];
 		labels[0] = "Transpose";
